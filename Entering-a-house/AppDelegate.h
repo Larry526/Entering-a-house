9b32d72b23fe8b9aa26df05de00a48d38fe97d0a
Larry Luk <larryluk526@gmail.com>
2017-11-08 13:48:41 -0500

    Initial Commit

---
+++ Entering-a-house/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Entering-a-house
+//
+//  Created by Larry Luk on 2017-11-08.
+//  Copyright © 2017 App App Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Entering-a-house/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Entering-a-house
+//
+//  Created by Larry Luk on 2017-11-08.
+//  Copyright © 2017 App App Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
